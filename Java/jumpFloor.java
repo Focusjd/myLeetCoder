3fa756e2f380088caf051793855ec0080124c1ce
focusjd <jiaodina0102@126.com>
2023-11-19 15:54:44 -0500

    jumpFloor2

--- Java/jumpFloor.java
+++ Java/jumpFloor.java
@@ -1,4 +1,5 @@
 public class jumpFloor {
+//   一只青蛙一次可以跳上1级台阶，也可以跳上2级。求该青蛙跳上一个n级的台阶(n为正整数)总共有多少种跳法。
 // Time: O(n), Space: O(1) similar to Fibonacci
     public int jumpFloor(int number) {
         if (number<=2) return number;
@@ -13,7 +14,24 @@ public class jumpFloor {
         return res;
     }
 
+//    一只青蛙一次可以跳上1级台阶，也可以跳上2级……它也可以跳上n级。求该青蛙跳上一个n级的台阶(n为正整数)总共有多少种跳法。
+//    Time: O(1) Space: O(1)
+//    public int jumpFloorII (int number) {
+//        return (int) Math.pow(2,  number-1);
+//    }
+//    Time: O(n) Space:P(n)
+    public int jumpFloorII (int number) {
+        int[] dp = new int[number+1];
+        dp[1] = 1;
+        for (int i = 2; i < number+1; i++) {
+            dp[i]=2 * dp[i-1];
+        }
+        return dp[number];
+    }
+
+
     public static void main(String[] args) {
         System.out.println(new jumpFloor().jumpFloor(7));
+        System.out.println(new jumpFloor().jumpFloorII(3));
     }
 }
