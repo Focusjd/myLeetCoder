024f6cdbea1dccdb36d7036569790cafe581e08e
focusjd <jiaodian0102@126.com>
2023-12-16 00:25:00 -0500

    update fast ReverseSentence

--- Java/MyString.java
+++ Java/MyString.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class MyString {
 
 //    给定一个长度为 n 的字符串，请编写一个函数判断该字符串是否回文。如果是回文请返回true，否则返回false。
@@ -27,6 +29,78 @@ public class MyString {
             sb.append(curr == ' ' ? "%20" : curr);
         }
         return sb.toString();
+    }
+
+
+//翻转单词序列
+// 输入：
+//"nowcoder. a am I"
+//返回值：
+//"I am a nowcoder."
+//    Easy Time n Space n
+//    public String ReverseSentence(String str) {
+//        String[] wordsList = str.split(" ");
+//        StringBuilder sb = new StringBuilder();
+//        for (int i = 0; i < wordsList.length; i++) {
+//            sb.append(wordsList[wordsList.length-1-i]);
+//            sb.append(" ");
+//        }
+//        return sb.toString().trim();
+//    }
+
+
+//    Time n Space 1
+//    public static String ReverseSentence(String str) {
+//        char[] chars = str.toCharArray();
+//        for (int i = 0; i < chars.length / 2; i++) {
+//            char temp = chars[i];
+//            chars[i] = chars[chars.length - 1 - i];
+//            chars[chars.length - 1 -i] = temp;
+//        }
+//
+//        int fast = 0;
+//        int slow = 0;
+//        for (int i = 0; i <= chars.length; i++) {
+//            if (i == chars.length || chars[i] == ' '){
+//                fast = i;
+//                for (int j = 0; j < (fast - slow) / 2; j++) {
+//                    char temp = chars[slow+j];
+//                    chars[slow+j] = chars[fast -1 -j];
+//                    chars[fast -1 -j] = temp;
+//                }
+//                slow = fast + 1;
+//            }
+//        }
+//        return String.valueOf(chars);
+//    }
+//    Time n Space 1
+    public static String ReverseSentence(String str) {
+        char[] chars = str.toCharArray();
+        reverseCharArray(chars, 0, chars.length-1);
+        int fast, slow =0;
+        for (int i = 0; i < chars.length + 1; i++) {
+            if (i == chars.length || chars[i] == ' '){
+                fast = i;
+                reverseCharArray(chars, slow, fast - 1);
+                slow = fast + 1;
+            }
+        }
+        return String.valueOf(chars);
+    }
+
+    public static void reverseCharArray(char[] chars, int start, int end){
+        while (start < end){
+            char temp = chars[start];
+            chars[start] = chars[end];
+            chars[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(ReverseSentence("This is a sample"));
     }
 
 }
