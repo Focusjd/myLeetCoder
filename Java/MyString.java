f999f0543663ee13d6a939b827c0993da7d2408d
focusjd <jiaodian0102@126.com>
2023-12-18 01:49:38 -0500

    update

--- Java/MyString.java
+++ Java/MyString.java
@@ -1,4 +1,7 @@
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class MyString {
 
@@ -122,11 +125,51 @@ public class MyString {
         }
     }
 
+//请实现一个函数用来找出字符流中第一个只出现一次的字符。例如，当从字符流中只读出前两个字符 "go" 时，第一个只出现一次的字符是 "g" 。
+// 当从该字符流中读出前六个字符 “google" 时，第一个只出现一次的字符是"l"。
+//    Medium Time 1 Space 1
+//    int[] list = new int[128];
+//    int index = 1;
+//    public void Insert(char ch){
+//        if (list[ch] != 0){
+//            list[ch] = -1;
+//        }else {
+//            list[ch] = index;
+//        }
+//        index++;
+//    }
+//    public char FirstAppearingOnce(){
+//        int res = Integer.MAX_VALUE;
+//        char resChar = '#';
+//        for (int i = 0; i < list.length; i++) {
+//            if (list[i] > 0 && list[i] < res){
+//                res = list[i];
+//                resChar = (char) i;
+//            }
+//        }
+//        return resChar;
+//    }
+//  Time n Space n
+    int[] charList = new int[128];
+    Queue<Character> queue = new LinkedList<>();
+    public void Insert(char ch){
+        charList[ch]++;
+        queue.offer(ch);
+    }
+    public char FirstAppearingOnce(){
+        while (!queue.isEmpty() && charList[queue.peek()] != 1){
+            queue.poll();
+        }
+        return queue.isEmpty() ? '#' : queue.peek();
+    }
+
+
 
 
 
     public static void main(String[] args) {
-        System.out.println(ReverseSentence("This is a sample"));
+//        System.out.println(ReverseSentence("This is a sample"));
+        System.out.println((int)'z');
     }
 
 }
