400d65b9e3cbce31cf774557990ecbd5538400ba
focusjd <jiaodian0102@126.com>
2023-12-18 02:28:09 -0500

    update

--- Java/MyArray.java
+++ Java/MyArray.java
@@ -163,13 +163,40 @@ public class MyArray {
         }
     }
 
-
     public Double GetMedian() {
         if ((minQ.size() + maxQ.size()) % 2 == 1){
             return (double) minQ.peek();
         }
         return (minQ.peek()+maxQ.peek())/2.0 ;
     }
+
+
+//给定一个长度为 n 的数组 num 和滑动窗口的大小 size ，找出所有滑动窗口里数值的最大值。
+//例如，如果输入数组{2,3,4,2,6,2,5,1}及滑动窗口的大小3，那么一共存在6个滑动窗口，他们的最大值分别为{4,4,6,6,6,5}；
+// 针对数组{2,3,4,2,6,2,5,1}的滑动窗口有以下6个： {[2,3,4],2,6,2,5,1}， {2,[3,4,2],6,2,5,1}， {2,3,[4,2,6],2,5,1}， {2,3,4,[2,6,2],5,1}， {2,3,4,2,[6,2,5],1}， {2,3,4,2,6,[2,5,1]}。
+//窗口大于数组长度或窗口长度为0的时候，返回空。
+//    !!!!!!!!!!!!!!
+    public ArrayList<Integer> maxInWindows (int[] num, int size) {
+        ArrayList<Integer> res = new ArrayList<>();
+        if (size > num.length || size == 0)
+            return res;
+        Queue<Integer> queue = new LinkedList<>();
+        PriorityQueue<Integer> pq = new PriorityQueue<>(size, Comparator.reverseOrder());
+        for (int i = 0; i < size; i++) {
+            queue.offer(num[i]);
+            pq.offer(num[i]);
+        }
+        res.add(pq.peek());
+        for (int i = size; i < num.length; i++) {
+            int removed = queue.poll();
+            pq.remove(removed); // need optimization
+            queue.add(num[i]);
+            pq.offer(num[i]);
+            res.add(pq.peek());
+        }
+        return res;
+    }
+
 
 
 }
