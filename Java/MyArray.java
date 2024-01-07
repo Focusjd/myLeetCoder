24fb8db467f5b97511cafdd1fbb8ea8007bee8af
focusjd <jiaodian0102@126.com>
2024-01-06 20:07:40 -0500

    update printNumbers

--- Java/MyArray.java
+++ Java/MyArray.java
@@ -424,7 +424,15 @@ public class MyArray {
         }
     }
 
-
+//输入数字 n，按顺序打印出从 1 到最大的 n 位十进制数。比如输入 3，则打印出 1、2、3 一直到最大的 3 位数 999。
+    public int[] printNumbers (int n) {
+        int upper = (int) Math.pow(10, n) -1;
+        int[] res = new int[upper];
+        for (int i = 1; i <= upper; i++) {
+            res[i-1] = i;
+        }
+        return res;
+    }
 
 
 }
